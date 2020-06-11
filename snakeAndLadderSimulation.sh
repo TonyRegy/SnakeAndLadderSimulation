a13e67c7e55ed2ccfc98c241b29ed34e522cef0c
TonyRegy <tonyregy@gmail.com>
2020-06-11 18:16:46 +0530

    ADDED:Function for deciding the move

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -3,7 +3,24 @@ COUNT=0
 currentPosition=0
 echo "snake and ladder game"
 echo "StartPosition=0"
+
 function rollDie(){
 s=$(($RANDOM%6 + 1))
+}
+
+function snakeLadderNoplay(){
+rollDie
+t=$(($RANDOM%3))
+case $t in
+0)
+        currentPosition=$currentPosition
+        ;;
+1)
+        currentPosition=$(($currentPosition + $s))
+        ;;
+2)
+        currentPosition=$(($currentPosition - $s))
+        ;;
+esac
 }
 
